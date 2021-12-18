9219fc293ec5ebb5d3a6623a2b47b35978e34b19
collinskoech11 <collinskoechck34@gmail.com>
2021-12-18 16:41:43 +0300

    migrate to mongodb

--- famotors/famotors/settings.py
+++ famotors/famotors/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = 'famotors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'demodatabase',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
